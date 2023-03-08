ab1d16e1f5934655a052066443b5b01f0772028d
Angel Gabriel Salvador Castañeda <02angelsalvador@gmail.com>
2023-03-08 16:34:34 -0600

    backend removed

--- api/response.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function responseRequest($code, $message, $finishConnection = false,$data = []) 
-    {
-      http_response_code($code);
-      echo json_encode(["status" => $code, "message" => $message,"data"=> (object) $data]);
-      if($finishConnection)
-        die();
-    }
